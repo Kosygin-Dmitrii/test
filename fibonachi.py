6c1217c3bea820d6c68c32155154f4f618a15ce7
Kosygin <Kosygin-Dmitrii@yandex.ru>
2020-11-27 22:23:03 +0300

    Second version

--- fibonachi.py
+++ fibonachi.py
@@ -14,3 +14,17 @@ def fib1(n):
 print(fib1(8))
 
 
+cache = {}  # Empty dictionary
+
+
+def fib2(n):
+    """
+    fib2 with cache
+    """
+    assert n >= 0
+    if n not in cache:
+        cache[n] = n if n <= 1 else fib2(n - 1) + fib2(n - 2)
+    return cache[n]
+
+
+print(fib2(800))
